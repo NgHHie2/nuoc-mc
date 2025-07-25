ea37d155a22e1431a9bae8c4e8bc5ec34355db78
NgHHie <hiep2003ka2@gmail.com>
2025-07-25 13:40:21 +0700

    kafka kraft và exception

--- accountservice/src/main/java/com/example/accountservice/config/OpenApiConfig.java
+++ accountservice/src/main/java/com/example/accountservice/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package com.example.accountservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,16 +11,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Account Service API")
-                        .description("API for Account Management")
-                        .version("1.0.0"))
-                // Point về Gateway để tránh CORS khi test API
-                .addServersItem(new Server()
-                        .url("http://localhost:8080")
-                        .description("API Gateway"));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                final String securitySchemeName = "bearerAuth";
+
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("Account Service API")
+                                                .description("API for Account Management")
+                                                .version("1.0.0"))
+                                // Point về Gateway để tránh CORS khi test API
+                                .addServersItem(new Server()
+                                                .url("http://localhost:8080")
+                                                .description("API Gateway"))
+                                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                                .components(new io.swagger.v3.oas.models.Components()
+                                                .addSecuritySchemes(securitySchemeName,
+                                                                new SecurityScheme()
+                                                                                .name(securitySchemeName)
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")));
+        }
 }
