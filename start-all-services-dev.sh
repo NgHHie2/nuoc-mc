937714239a6c32e9697c8730078bc9e02eb1efe2
NgHHie <hiep2003ka2@gmail.com>
2025-07-29 16:12:43 +0700

    chỉnh sửa 1 số cấu hình build

--- start-all-services-dev.sh
+++ start-all-services-dev.sh
@@ -2,22 +2,22 @@
 
 # Run API Gateway container
 cd api-gateway
-docker compose -f docker-compose.dev.yml up -d
+docker compose -f docker-compose.staging.yml up -d
 cd ..
 
 # Run Account Service container
 cd account-service
-docker compose -f docker-compose.dev.yml up -d
+docker compose -f docker-compose.staging.yml up -d
 cd ..
 
 # Run Learn Service container
 cd learn-service
-docker compose -f docker-compose.dev.yml up -d
+docker compose -f docker-compose.staging.yml up -d
 cd ..
 
 # Run Stats Service container
 cd stats-service
-docker compose -f docker-compose.dev.yml up -d
+docker compose -f docker-compose.staging.yml up -d
 cd ..
 
 echo "All services run successfully!"
